21f5b4d0f75f3b180ed468366789bb1133325582
Abhishek <abhi885990@gmail.com>
2023-03-30 16:45:35 +0530

    added env file

--- src/Firebase/FirebaseAuth.js
+++ src/Firebase/FirebaseAuth.js
@@ -7,13 +7,21 @@ import {
   signOut,
 } from "firebase/auth";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCkKH17a7RIVpWeocirKgF32RMerJj_lYc",
+//   authDomain: "devs-for-dev.firebaseapp.com",
+//   projectId: "devs-for-dev",
+//   storageBucket: "devs-for-dev.appspot.com",
+//   messagingSenderId: "404910649536",
+//   appId: "1:404910649536:web:2dbb4f49ffb535fbefbd25",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCkKH17a7RIVpWeocirKgF32RMerJj_lYc",
-  authDomain: "devs-for-dev.firebaseapp.com",
-  projectId: "devs-for-dev",
-  storageBucket: "devs-for-dev.appspot.com",
-  messagingSenderId: "404910649536",
-  appId: "1:404910649536:web:2dbb4f49ffb535fbefbd25",
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSENGER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
 };
 
 const app = initializeApp(firebaseConfig);
